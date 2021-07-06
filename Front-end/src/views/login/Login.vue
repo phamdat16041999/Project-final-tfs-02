be4eb13b35184a727a4803c08806fd8e01ad6984
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2021-07-06 14:27:54 +0700

    Chia component login

--- Front-end/src/views/login/Login.vue
+++ Front-end/src/views/login/Login.vue
@@ -54,81 +54,8 @@
       <div class="row">
         <div class="col-xl-2 col-12"></div>
         <div id="formInput" v-bind:class="leftToRightData.formInput">
-          <div class="row" id="signUp" v-if="signUp">
-            <div class="col-xl-2 col-12"></div>
-            <div class="col-xl-8 col-12">
-              <div class="row">
-                <div class="col-6">
-                  <label>First name:</label>
-                  <input type="text" class="form-control" />
-                </div>
-                <div class="col-6">
-                  <label>Last name:</label>
-                  <input type="text" class="form-control" />
-                </div>
-                <div class="col-6">
-                  <label>Date of birth</label>
-                  <input type="date" class="form-control" />
-                </div>
-                <div class="col-6">
-                  <label>Phone</label>
-                  <input type="number" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>Address</label>
-                  <input type="text" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>Email</label>
-                  <input type="email" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>User name</label>
-                  <input type="text" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>Passwod</label>
-                  <input type="password" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>Confirm password</label>
-                  <input type="password" class="form-control" />
-                </div>
-                <div
-                  class="col-12"
-                  style="text-align: center; margin-top: 10px"
-                >
-                  <button type="button" class="btn btn-light">Sign up</button>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-2 col-12"></div>
-          </div>
-          <div class="row" id="login" v-if="login">
-            <div class="col-xl-2 col-12"></div>
-            <div class="col-xl-8 col-12">
-              <div class="row">
-                <div class="col-12" style="text-align: center">
-                  <h3>Login</h3>
-                </div>
-                <div class="col-12">
-                  <label>User name</label>
-                  <input type="text" class="form-control" />
-                </div>
-                <div class="col-12">
-                  <label>Passwod</label>
-                  <input type="password" class="form-control" />
-                </div>
-                <div
-                  class="col-12"
-                  style="text-align: center; margin-top: 10px"
-                >
-                  <button type="button" class="btn btn-light">Login</button>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-2 col-12"></div>
-          </div>
+          <signUpForm id="signUp" v-if="signUp"/>
+          <loginForm id="login" v-if="login"/>
         </div>
         <div class="col-4"></div>
         <div class="col-2"></div>
@@ -138,8 +65,13 @@
 </template>
 
 <script>
+import loginForm from "./FormLogin.vue";
+import signUpForm from "./SignUpForm.vue";
 export default {
-  name: "HelloWorld",
+  components:{
+    loginForm,
+    signUpForm
+  },
   data() {
     return {
       login: false,
