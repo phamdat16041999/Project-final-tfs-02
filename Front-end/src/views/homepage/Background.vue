bca7498223fbe92476e7435587f3c15dff22f653
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2021-07-07 15:30:38 +0700

    aaaa

--- Front-end/src/views/homepage/Background.vue
+++ Front-end/src/views/homepage/Background.vue
@@ -7,19 +7,26 @@
       <div class="row">
         <div class="col-12 checkIn">
           <div class="row">
-            <div class="col-xl-3 col-12">
+            <div class="col-xl-4 col-12">
               <label>City</label>
               <select class="form-control">
                 <option v-for="post,indext in posts" :key="indext">{{post.address}}</option>
               </select>
             </div>
-            <div class="col-xl-3 col-12">
-              <label>Check in</label>
-              <input type="date" class="form-control" />
+            <div class="col-xl-5 col-12">
+              <label >Star</label>
+               <div class="rating-css">
+                 <input type="radio" name="rating1" id="rating1">
+                 <label for="rating1" class="fa fa-star"></label>
+                 <input type="radio" name="rating1" id="rating2">
+                <label for="rating2" class="fa fa-star"></label>
+                <input type="radio" name="rating1" id="rating3">
+                <label for="rating3" class="fa fa-star"></label>
+                <input type="radio" name="rating1" id="rating4">
+                <label for="rating4" class="fa fa-star"></label>
+                <input type="radio" name="rating1" id="rating5">
+                <label for="rating5" class="fa fa-star"></label>
             </div>
-            <div class="col-xl-3 col-12">
-              <label>Check out</label>
-              <input type="date" class="form-control" />
             </div>
             <div class="col-xl-3 col-12">
               <label></label>
@@ -59,6 +66,29 @@ export default {
 };
 </script>
 <style scoped>
+.rating-css {
+  color: #ffe400;
+  font-size: 30px;
+  font-family: sans-serif;
+  font-weight: 800;
+  text-align: center;
+  text-transform: uppercase;
+}
+.rating-css input {
+  display: none;
+}
+.rating-css input + label {
+  font-size: 30px;
+  text-shadow: 1px 1px 0 #ffe400;
+  cursor: pointer;
+}
+.rating-css input:checked + label ~ label {
+  color: #838383;
+}
+.rating-css label:active {
+  transform: scale(0.8);
+  transition: 0.3s ease;
+}
 .slogan {
   color: white;
   text-align: center;
