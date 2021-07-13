8e607edab831353ad30a76e476f15e66e2fc3bfe
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2021-07-13 11:22:29 +0700

    update code

--- Back-end/router/router.go
+++ Back-end/router/router.go
@@ -26,7 +26,7 @@ func Run() {
 	//get method
 	get := r.Methods(http.MethodGet).Subrouter()
 	get.Path("/homepage").HandlerFunc(model.DataHomePage)
-	get.Path("/hotel/{address}").HandlerFunc(model.GetHotelAddress)
+	get.Path("/homepage/hotel/{address}/{rate}").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.SeachHotelAddress))
 	get.Path("/tophotel").HandlerFunc(model.GetTopHotel)
 	get.Path("/detailhotel/{id}").HandlerFunc(model.GetDetailHotel)
 
