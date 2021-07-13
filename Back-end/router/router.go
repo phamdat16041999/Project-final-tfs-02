98539536b8c4fec2ed6ef13afc8caa5c3a0316a5
Mr.Victory1509 <55972167+mrvictory1509@users.noreply.github.com>
2021-07-13 11:11:45 +0700

    a

--- Back-end/router/router.go
+++ Back-end/router/router.go
@@ -19,16 +19,14 @@ func Run() {
 	post.Path("/changepassword").HandlerFunc(model.ChangePassword)
 	post.Path("/active").HandlerFunc(model.ActiveAccount)
 	post.Path("/createbill").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.Createbill))
-	post.Path("/createhotel").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.CreateHotel))
+	post.Path("/rating").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.Rating))
+	post.Path("/checkroomstatus").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.Checkroomstatus))
+	// post.Path("/payment").HandlerFunc(middlewares.SetMiddlewareAuthentication(model.Payment))
 
 	//get method
 	get := r.Methods(http.MethodGet).Subrouter()
 	get.Path("/homepage").HandlerFunc(model.DataHomePage)
-<<<<<<< HEAD
 	get.Path("/hotel/{address}").HandlerFunc(model.GetHotelAddress)
-=======
-	get.Path("/homepage/hotel/{address}/{rate}").HandlerFunc(model.GetHotelAddress)
->>>>>>> d6d66811dd3574ee337fb74d350d3d08f074d059
 	get.Path("/tophotel").HandlerFunc(model.GetTopHotel)
 	get.Path("/detailhotel/{id}").HandlerFunc(model.GetDetailHotel)
 
@@ -42,7 +40,7 @@ func Run() {
 	handler := cors.New(cors.Options{
 		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS", "PUT"},
 	}).Handler(r)
-	http.ListenAndServe(":8000", handler)
+	http.ListenAndServe(":8080", handler)
 }
 func Test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "test")
