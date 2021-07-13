33ee18d6879a699bf539c42cf05c4869171025cf
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2021-07-13 17:21:08 +0700

    update chat

--- Back-end/model/messenger.go
+++ Back-end/model/messenger.go
@@ -1,10 +1,31 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"hotel/connect"
+	"strconv"
+
+	"gorm.io/gorm"
+)
 
 type Messenger struct {
 	gorm.Model
 	UserID         uint   `json:"userID"`
 	Messenger      string `gorm:"type:text;" json:"messenger"`
 	ConversationID uint   `json:"conversationID"`
+}
+
+func CreateMessenger(UserID, messenger string, conversationID uint) []Messenger {
+	db := connect.Connect()
+	id, _ := strconv.ParseUint(UserID, 10, 64)
+	if messenger != "" {
+		messeng := Messenger{
+			UserID:         uint(id),
+			Messenger:      messenger,
+			ConversationID: conversationID,
+		}
+		db.Create(&messeng)
+	}
+	var mess []Messenger
+	db.Debug().Where("conversation_id = ?", conversationID).Find(&mess)
+	return mess
 }
